cff09c404d704e1542ac2a40db7dc7451cf3107d
mksigrest <mksigrest@gmail.com>
2024-10-24 09:06:40 -0400

    final update 10/24/24
    started working on base and class constructors but never finished

--- classes_structs_oop/classes_structs_oop/Program.cs
+++ classes_structs_oop/classes_structs_oop/Program.cs
@@ -12,10 +12,23 @@
         public Character(String name)
         {
             this.name = name;
+            Reset();
         }
         public void PrintStatsInfo()
         {
             Console.WriteLine("Hero: " + this.name + " - " + this.exp + " EXP");
+        }
+        private void Reset()
+        {
+            this.name = "Not assigned";
+            this.exp = 0;
+        }
+    }
+    public class Paladin: Character
+    {
+        public Paladin(String name): Character(name)
+        {
+
         }
     }
     public struct Weapon
@@ -45,9 +58,30 @@
             heroine.PrintStatsInfo();
 
             Weapon huntingBow = new Weapon("Hunting Bow", 105);
-            {
-                huntingBow.PrintWeaponStats();
-            }
+            huntingBow.PrintWeaponStats();
+
+            Character villian = hero;
+
+            hero.PrintStatsInfo();
+            villian.PrintStatsInfo();
+
+            villian.name = "Sir Kane the Brave";
+
+            hero.PrintStatsInfo();
+            villian.PrintStatsInfo();
+
+            Weapon warBow = huntingBow;
+
+            huntingBow.PrintWeaponStats();
+            warBow.PrintWeaponStats();
+
+            warBow.name = "War Bow";
+            warBow.damage = 155;
+
+            huntingBow.PrintWeaponStats();
+            warBow.PrintWeaponStats();
+
+            hero.Reset();
         }
     }
 }
